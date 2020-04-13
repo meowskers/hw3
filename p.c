a74981045d1f021cf1fca1519d9d12904cfab2b3
edwinwallis <wallie@rpi.edu>
2020-04-13 14:03:10 -0400

    sdf

--- p.c
+++ p.c
@@ -1,16 +1,17 @@
-1 void * q3( void * y ) 2{
-3 int*x=(void*)y;
-4   char * s = calloc( *x + 16, sizeof( char ) );
-5   sprintf( s, "ABCDEFGH" );
-6   fprintf( stderr, "%s", s );
-7   return NULL;
-8}
-9 int main()
-10 {
-11   int * z = calloc( 1, sizeof( int ) );
-12   if (fork() == 0) *z = 10;
-13 pthread_t t1;
-14 pthread_create( &t1, NULL, q3, z );
-15   fprintf( stderr, "ERROR" );
-16 pthread_join( t1, NULL );
-17 return EXIT_SUCCESS;
+void * q3( void * y ) 2{
+int*x=(void*)y;
+ char * s = calloc( *x + 16, sizeof( char ) );
+sprintf( s, "ABCDEFGH" );
+  fprintf( stderr, "%s", s );
+ return NULL;
+}
+ int main()
+ {
+   int * z = calloc( 1, sizeof( int ) );
+   if (fork() == 0) *z = 10;
+ pthread_t t1;
+ pthread_create( &t1, NULL, q3, z );
+   fprintf( stderr, "ERROR" );
+ pthread_join( t1, NULL );
+ return 0;
+}
